a3aed793557ef5789d124e0735ca097f3fd58971
Nurbek Sirojiddinov <nurbek.sirojiddinov@livetalks.tv>
2022-12-01 23:47:11 +0500

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.7.6"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2021.0.5"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    // https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-oauth2
+    implementation("org.springframework.cloud:spring-cloud-starter-oauth2:2.2.5.RELEASE")
+    implementation("org.projectlombok:lombok:1.18.22")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    compileOnly("org.springframework.security:spring-security-web")
+    compileOnly("org.springframework.security:spring-security-config")
+    implementation("com.auth0:java-jwt:4.2.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
